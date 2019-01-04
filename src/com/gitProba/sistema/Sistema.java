6aa2c06506b985d972bdb08a7aa0ed39ff8d99d7
nere <nere@nere-Lenovo-G510>
2019-01-04 21:36:22 +0100

    commit no.2 (Comparación entre números)

--- src/com/gitProba/sistema/Sistema.java
+++ src/com/gitProba/sistema/Sistema.java
@@ -1,10 +1,23 @@
 package com.gitProba.sistema;
 
+import java.util.Scanner;
+
 public class Sistema {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Kaixo mundu hori!");
+		Scanner entrada= new Scanner(System.in);
+		System.out.println("Introduce un número");
+		int num1=entrada.nextInt();
+		System.out.println("Introduce otro número");
+		int num2=entrada.nextInt();
+		if (num1<num2) {
+			System.out.println("El "+num2+" es mayor que el "+ num1+".");
+		}
+		else {
+			System.out.println("El "+num1+" es mayor que el "+ num2+".");
+		}
+		
 
 	}
 
