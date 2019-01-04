b67fb21c9114fee02ddf5916d26d8abc80eea0cc
nere <nere@nere-Lenovo-G510>
2019-01-04 21:23:14 +0100

    Commit no. 2 (Imprime mensaje)

--- src/com/gitProba/sistema/Sistema.java
+++ src/com/gitProba/sistema/Sistema.java
@@ -3,7 +3,8 @@ package com.gitProba.sistema;
 public class Sistema {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Kaixo mundu polit hori!");
 
 	}
 
