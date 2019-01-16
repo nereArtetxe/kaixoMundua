f85e9ed5a00bbe91d73fc506b5791c07c8f63a3b
infnartetxe <infnartetxe@HZ327437.H015112.NET>
2019-01-16 15:57:20 +0100

    Nartetxeren kommita

--- src/com/gitProba/sistema/Sistema.java
+++ src/com/gitProba/sistema/Sistema.java
@@ -6,19 +6,17 @@ public class Sistema {
 
 	public static void main(String[] args) {
 		
-	System.out.println("Y ahora qué?");
-	System.out.println("Eta beste mundu bat sortu nahi badugu???");
-	int x=3;
-	if (x<5)
-	{
-		System.out.println("hau da sartuko dena");
-	}
-	else
-	{
-		System.out.println("Aukera hau ez da inoiz ere aterako");
-		
-		
-	}
+		System.out.println("Y ahora qué?");
+		System.out.println("Eta beste mundu bat sortu nahi badugu???");
+		int x=3;
+		if (x<5)
+		{
+			System.out.println("hau da sartuko dena");
+		}
+		else
+		{
+			System.out.println("Aukera hau ez da inoiz ere aterako");
+		}
 	}
 
 }
