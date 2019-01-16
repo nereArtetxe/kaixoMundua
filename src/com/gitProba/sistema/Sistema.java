59fbc7c06ec0b45098d97562ba7c811c31958637
infnartetxe <infnartetxe@HZ327437.H015112.NET>
2019-01-16 15:53:37 +0100

    Hauxe da nartetxe bezala egin dudan aportazioa, Elorrietan, 15:53tan

--- src/com/gitProba/sistema/Sistema.java
+++ src/com/gitProba/sistema/Sistema.java
@@ -7,6 +7,16 @@ public class Sistema {
 	public static void main(String[] args) {
 		
 	System.out.println("Y ahora qué?");
+	System.out.println("Eta beste mundu bat sortu nahi badugu???");
+	int x=3;
+	if (x<5)
+	{
+		System.out.println("hau da sartuko dena");
+	}
+	else
+	{
+		System.out.println("Aukera hau ez da inoiz ere aterako");
+	}
 	}
 
 }
